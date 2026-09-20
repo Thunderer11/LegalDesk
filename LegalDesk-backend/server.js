9dee5adb18fc6e2fc0840891f0922e999b434529
Samarth Sharma <samarthsharma1411@gmail.com>
2026-09-20 22:22:55 +0530

    add blog creation API

--- LegalDesk-backend/server.js
+++ LegalDesk-backend/server.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 const app = express();
 const PORT = 5000;
+app.use(express.json());
 
 const sql = neon(process.env.DATABASE_URL);
 
@@ -33,7 +34,53 @@ app.get("/api/blogs", async (req, res) => {
         });
     }
 });
+app.post("/api/blogs", async (req, res) => {
+    try {
+        const {
+            title,
+            slug,
+            excerpt,
+            content,
+            category,
+            cover_image,
+            author,
+            published
+        } = req.body;
 
+        const newBlog = await sql`
+            INSERT INTO blogs (
+                title,
+                slug,
+                excerpt,
+                content,
+                category,
+                cover_image,
+                author,
+                published
+            )
+            VALUES (
+                ${title},
+                ${slug},
+                ${excerpt},
+                ${content},
+                ${category},
+                ${cover_image},
+                ${author || "Adv. Samridhi Sharma"},
+                ${published || false}
+            )
+            RETURNING *
+        `;
+
+        res.status(201).json(newBlog[0]);
+
+    } catch (error) {
+        console.error("Error creating blog:", error);
+
+        res.status(500).json({
+            message: "Failed to create blog."
+        });
+    }
+});
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
