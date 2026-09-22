99e21c0c01f47911a4ac60bfb31959d88015d756
Samarth Sharma <samarthsharma1411@gmail.com>
2026-09-22 12:42:20 +0530

    Prepare backend for Vercel deployment

--- LegalDesk-backend/server.js
+++ LegalDesk-backend/server.js
@@ -11,7 +11,7 @@ const sanitizeHtml = require("sanitize-html");
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
     origin: true,
